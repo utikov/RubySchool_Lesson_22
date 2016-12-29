9ed56020155f6e8de619a299fc2caf57b5c36eee
Ivan Utikov <ivanutikov@MacBook-Pro-Ivan.local>
2016-12-29 16:36:51 +0200

    Add "about.erb"

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"			
 end
 
-get '/about' do
+get '/aboutus' do
 	erb :about
 end
 
